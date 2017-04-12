00d88d7bf8e1596b8b9deb385e2fa959cb3600f2
Arya Pradipta <aryapradipta9@gmail.com>
2017-04-12 07:31:53 +0700

    DELETE

--- src/Main.java
+++
@@ -1,6 +0,0 @@
-/**
- * Created by lenovo on 4/12/2017.
- */
-public class Main {
-    
-}
--- src/zaff/Main.java
+++
@@ -1,8 +0,0 @@
-package zaff;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
