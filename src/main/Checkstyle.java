332f10034a917365cf4047074f8e8c118ceee301
Arya Pradipta <aryapradipta9@gmail.com>
2017-04-12 08:10:05 +0700

    tambahkan template checkstyle

---
+++ src/main/Checkstyle.java
@@ -0,0 +1,25 @@
+package main;
+
+/**
+ * Created by 13515017 / Putu Arya Pradipta
+ * Tanggal 4/12/2017.
+ * FileName : Checkstyle.java
+ */
+public class Checkstyle {
+  private int sizeX;
+
+  /**
+   * Konstruktor tanpa parameter.
+   */
+  public Checkstyle() {
+    sizeX = 10;
+  }
+
+  /**
+   * Getter Size.
+   * @return ukuran X yang tersimpan
+   */
+  public int getSizeX() {
+    return sizeX;
+  }
+}
