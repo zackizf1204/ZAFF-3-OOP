db434ed8f907c3aee4dfd890b0f1fac04900f770
Arya Pradipta <aryapradipta9@gmail.com>
2017-04-13 09:42:38 +0700

    tambah google style

---
+++ src/view/CommandView.java
@@ -0,0 +1,14 @@
+import javax.swing.*;
+
+package view;
+
+/**
+ * Created by 13515017 / Putu Arya Pradipta.
+ * Tanggal 4/13/2017.
+ * FileName : CommandView.java.
+ */
+public class CommandView extends JButton {
+  public CommandView() {
+    super
+  }
+}
---
+++ src/view/EventListen.java
@@ -0,0 +1,9 @@
+package view;
+
+/**
+ * Created by 13515017 / Putu Arya Pradipta.
+ * Tanggal 4/13/2017.
+ * FileName : EventListen.java.
+ */
+public class EventListen {
+}
