058bc15469ca7fb9c0ec50e3f98cd0d72e55cb08
fathur2707 <mfathur2707@gmail.com>
2017-04-13 19:17:37 +0900

    commit prototipe

--- src/object/Player.java
+++ src/object/Player.java
@@ -2,7 +2,25 @@ package object;
 
 /**
  * Created by Fathur on 13-Apr-17.
- * 13515068.
+ * NIM : 13515068
+ * File : Player.java
  */
 public class Player {
+  private Unit[] arrayUnit;
+  private String playerName;
+
+  public Player(String input){
+    playerName = input;
+    arrayUnit = new Unit [10];
+    arrayUnit[0] = new Unit();
+  }
+
+  public Player(String input,int type){
+    playerName = input;
+    arrayUnit = new Unit [10];
+    arrayUnit[0] = new Unit(type);
+  }
+
+
 }
+
--- src/object/Unit.java
+++ src/object/Unit.java
@@ -2,8 +2,48 @@ package object;
 
 /**
  * Created by Fathur on 13-Apr-17.
- * 13515068.
+ * NIM : 13515068
+ * File : Unit.java
  */
-public class Unit {
+public class Unit extends MapObject {
+  private int maxHp;
+  private int maxMp;
+  private int currentHp;
+  private int currentMp;
+  private int strength;
+  private int intelligence;
+  private int mov;
+  private int agi;
 
+  public Unit(){
+
+  }
+
+  public Unit(int mhp, int mmp, int strength, int intelligence, int mov, int agi, int absis,int ordinat){
+
+  }
+
+  public Unit(int type){
+
+  }
+
+  public void Attack(Unit target){
+
+  }
+
+  public void Move(int i){
+
+  }
+
+  public void Skill(){
+
+  }
+
+  public void Wait(){
+
+  }
+
+  public void Pick(){
+
+  }
 }
