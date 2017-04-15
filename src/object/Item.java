514dd402af87cc5eee1d020d4ec85709ff3af640
zackizf1204 <zackizf1204@gmail.com>
2017-04-15 14:13:14 +0700

    mapobject

---
+++ src/object/Item.java
@@ -0,0 +1,13 @@
+package object;
+
+/**
+ * Created by Zacki Zulfikar Fauzi on 13-Apr-17.
+ * NIM  : 13515147
+ * File : Item.java
+ */
+public class Item {
+  private int addMaxHp;
+  private int addMaxMp;
+  private int addStrength;
+  private int addIntelligence;
+}
---
+++ src/object/MapObject.java
@@ -0,0 +1,39 @@
+package object;
+
+/**
+ * Created by Zacki Zulfikar Fauzi on 13-Apr-17.
+ * NIM  : 13515147
+ * File : MapObject.java
+ */
+public class MapObject {
+  private int absis;
+  private int ordinat;
+
+  public MapObject() {
+    absis = 0;
+    ordinat = 0;
+  }
+
+
+  public MapObject(int absis, int ordinat) {
+    this.absis = absis;
+    this.ordinat = ordinat;
+  }
+
+  public int getAbsis() {
+    return absis;
+  }
+
+  public int getOrdinat() {
+    return ordinat;
+  }
+
+  public void setAbsis(int absis) {
+    this.absis = absis;
+  }
+
+  public void setOrdinat(int ordinat) {
+    this.ordinat = ordinat;
+  }
+
+}
