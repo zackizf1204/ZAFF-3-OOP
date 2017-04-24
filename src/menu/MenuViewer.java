2f0ece2e04213e29ccc7a5e9aef06ddd61d8fc47
Finiko <mybeddrom@gmail.com>
2017-04-24 19:53:51 +0700

    Merge branches 'Finiko' and 'view-arya' of https://github.com/zackizf1204/ZAFF-3-OOP into view-arya
    # Conflicts:
    #	src/view/map/MapViewer.java

--- src/menu/MenuViewer.java
+++ src/menu/MenuViewer.java
@@ -11,35 +11,52 @@ import java.io.IOException;
  */
 public class MenuViewer extends JPanel{
   private JLayeredPane menuPanel;
+  private JLayeredPane backgroundPanel;
   private JButton playButton;
   private JButton exitButton;
   private GridBagConstraints batasan;
   private GridBagLayout menuLayout;
-  private JComponent content;
+  private GridBagLayout backgroundLayout;
+  private JLayeredPane content;
   private Integer[] pilihanJumlah;
   private JLabel pilihanLabel;
   private JComboBox pilihanBox;
+  private JLabel background;
   private int inputPlayer;
+  private BufferedImage image;
 
   public MenuViewer () {
     batasan = new GridBagConstraints();
     menuLayout = new GridBagLayout();
+    backgroundLayout = new GridBagLayout();
     menuPanel = new JLayeredPane();
-    content = new JPanel(menuLayout);
+    backgroundPanel = new JLayeredPane();
+    content = new JLayeredPane();
+    content.setLayout(backgroundLayout);
     menuPanel.setLayout(menuLayout);
+    backgroundPanel.setLayout(backgroundLayout);
+    buildBackground();
     buildPilihanJumlah();
     buildPlayButton();
     buildExitButton();
     content.setOpaque(true);
+    batasan.gridx = 0;
+    batasan.gridy = 0;
+
+    content.add(backgroundPanel);
+    backgroundLayout.setConstraints(backgroundPanel,batasan);
+    content.setLayer(backgroundPanel,new Integer(0),0);
     content.add(menuPanel);
+    backgroundLayout.setConstraints(menuPanel,batasan);
+    content.setLayer(menuPanel,new Integer(1),0);
   }
   public void menuShow() {
     JFrame frame = new JFrame("ZAFF");
     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     frame.add(content);
     //frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-    frame.setUndecorated(true);
-    frame.setSize(1000,100);
+    //frame.setUndecorated(true);
+    frame.pack();
     frame.setVisible(true);
   }
   public void buildPilihanJumlah() {
@@ -77,8 +94,27 @@ public class MenuViewer extends JPanel{
     menuPanel.add(exitButton);
     menuPanel.setLayer(exitButton,new Integer(1),0);
   }
+  public void buildBackground() {
+    try {
+      batasan.gridx = 0;
+      batasan.gridy = 0;
+      image = ImageIO.read(getClass().getResource("../assets/main_menu.jpg"));
+      background = new JLabel(new ImageIcon(image));
+      backgroundLayout.setConstraints(background,batasan);
+      backgroundPanel.add(background);
+      backgroundPanel.setLayer(background,new Integer(0),0);
+    } catch (IOException ie){
+      System.out.println("Error:" + ie.getMessage());
+    }
+  }
   public int getInputPlayer() {
     inputPlayer = (int) pilihanBox.getSelectedItem();
     return (inputPlayer);
+  }
+  public JButton getPlayButton() {
+    return(playButton);
+  }
+  public JButton getExitButton() {
+    return(exitButton);
   }
 }
