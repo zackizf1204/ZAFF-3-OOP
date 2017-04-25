29b04b0a0c075c370065c5c1ab09f8cc3ee4dfb8
zackizf1204 <zackizf1204@gmail.com>
2017-04-25 21:47:05 +0700

    Merge branch 'view-arya' of https://github.com/zackizf1204/ZAFF-3-OOP into Virtuoso
    # Conflicts:
    #	src/DriverModel.java
    #	src/object/Unit.java

--- src/menu/MenuViewer.java
+++ src/menu/MenuViewer.java
@@ -12,17 +12,17 @@ import java.io.IOException;
 public class MenuViewer extends JPanel{
   private JLayeredPane menuPanel;
   private JLayeredPane backgroundPanel;
-  public JButton playButton;
-  public JButton exitButton;
+  private JButton playButton;
+  private JButton exitButton;
   private GridBagConstraints batasan;
   private GridBagLayout menuLayout;
   private GridBagLayout backgroundLayout;
   private JLayeredPane content;
-  private Integer[] pilihanJumlah;
-  private JLabel pilihanLabel;
-  private JComboBox pilihanBox;
+  private JComboBox playerBox;
   private JLabel background;
   private int inputPlayer;
+  private int map;
+  private JComboBox mapBox;
   private BufferedImage image;
 
   public MenuViewer () {
@@ -36,13 +36,13 @@ public class MenuViewer extends JPanel{
     menuPanel.setLayout(menuLayout);
     backgroundPanel.setLayout(backgroundLayout);
     buildBackground();
+    buildPilihanMap();
     buildPilihanJumlah();
     buildPlayButton();
     buildExitButton();
     content.setOpaque(true);
     batasan.gridx = 0;
     batasan.gridy = 0;
-
     content.add(backgroundPanel);
     backgroundLayout.setConstraints(backgroundPanel,batasan);
     content.setLayer(backgroundPanel,new Integer(0),0);
@@ -60,36 +60,64 @@ public class MenuViewer extends JPanel{
     frame.setVisible(true);
   }
   public void buildPilihanJumlah() {
-    Integer[] pilihanJumlah;
-    pilihanJumlah = new Integer[3];
-    pilihanJumlah[0] = 2;
-    pilihanJumlah[1] = 3;
-    pilihanJumlah[2] = 4;
-    pilihanLabel = new JLabel("<html><font color='yellow'>Jumlah Player : </font></html>");
-    pilihanBox = new JComboBox(pilihanJumlah);
-    batasan.gridx = 1;
-    batasan.gridy = 0;
-    menuLayout.setConstraints(pilihanLabel,batasan);
-    menuPanel.add(pilihanLabel);
-    menuPanel.setLayer(pilihanLabel,new Integer(1),0);
-    batasan.gridx = 1;
-    batasan.gridy = 1;
-    menuLayout.setConstraints(pilihanBox,batasan);
-    menuPanel.add(pilihanBox);
-    menuPanel.setLayer(pilihanBox,new Integer(1),0);
-  }
-  public void buildPlayButton() {
-    playButton = new JButton("play");
+    JLabel playerLabel;
+    playerLabel = new JLabel("PLAYERS : ");
+    playerLabel.setFont(new Font("Arial",Font.BOLD,15));
+    Integer[] pilihanPlayer;
+    pilihanPlayer = new Integer[3];
+    pilihanPlayer[0] = 2;
+    pilihanPlayer[1] = 3;
+    pilihanPlayer[2] = 4;
+    playerBox = new JComboBox(pilihanPlayer);
+    playerBox.setFont(new Font("Arial",Font.PLAIN,15));
     batasan.gridx = 0;
     batasan.gridy = 0;
+    menuLayout.setConstraints(playerLabel,batasan);
+    menuPanel.add(playerLabel);
+    menuPanel.setLayer(playerLabel,new Integer(1),0);
+    batasan.gridx = 1;
+    batasan.gridy = 0;
+    menuLayout.setConstraints(playerBox,batasan);
+    menuPanel.add(playerBox);
+    menuPanel.setLayer(playerBox,new Integer(1),0);
+  }
+  public void buildPilihanMap() {
+    JLabel mapLabel;
+    mapLabel = new JLabel("MAP :");
+    mapLabel.setFont(new Font("Arial",Font.BOLD,15));
+    Integer[] pilihanMap;
+    pilihanMap = new Integer[2];
+    pilihanMap[0] = 1;
+    pilihanMap[1] = 2;
+    mapBox = new JComboBox(pilihanMap);
+    mapBox.setFont(new Font("Arial",Font.PLAIN,15));
+    batasan.gridx = 0;
+    batasan.gridy = 1;
+    menuLayout.setConstraints(mapLabel,batasan);
+    menuPanel.add(mapLabel);
+    menuPanel.setLayer(mapLabel,new Integer(1),0);
+    batasan.gridx = 1;
+    batasan.gridy = 1;
+    menuLayout.setConstraints(mapBox,batasan);
+    menuPanel.add(mapBox);
+    menuPanel.setLayer(mapBox,new Integer(1),0);
+  }
+  public void buildPlayButton() {
+    playButton = new JButton("PLAY");
+    playButton.setFont(new Font("Arial",Font.PLAIN,30));
+    playButton.setPreferredSize(new Dimension(120,40));
+    batasan.gridx = 0;
+    batasan.gridy = 2;
     menuLayout.setConstraints(playButton,batasan);
     menuPanel.add(playButton);
     menuPanel.setLayer(playButton,new Integer(1),0);
   }
   public void buildExitButton() {
-    exitButton = new JButton("exit");
+    exitButton = new JButton("EXIT");
+    exitButton.setFont(new Font("Arial",Font.PLAIN,30));
+    exitButton.setPreferredSize(new Dimension(120,40));
     batasan.gridx = 0;
-    batasan.gridy = 1;
+    batasan.gridy = 3;
     menuLayout.setConstraints(exitButton,batasan);
     menuPanel.add(exitButton);
     menuPanel.setLayer(exitButton,new Integer(1),0);
@@ -98,7 +126,7 @@ public class MenuViewer extends JPanel{
     try {
       batasan.gridx = 0;
       batasan.gridy = 0;
-      image = ImageIO.read(getClass().getResource("../assets/main_menu.jpg"));
+      image = ImageIO.read(getClass().getResource("../assets/menutitle_2.jpg"));
       background = new JLabel(new ImageIcon(image));
       backgroundLayout.setConstraints(background,batasan);
       backgroundPanel.add(background);
@@ -108,8 +136,12 @@ public class MenuViewer extends JPanel{
     }
   }
   public int getInputPlayer() {
-    inputPlayer = (int) pilihanBox.getSelectedItem();
+    inputPlayer = (int) playerBox.getSelectedItem();
     return (inputPlayer);
+  }
+  public int getMap() {
+    map = (int) mapBox.getSelectedItem();
+    return (map);
   }
   public JButton getPlayButton() {
     return(playButton);
